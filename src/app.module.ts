a37022afd195fb99b0820dae2a13263afd048fea
Thomassr <thomassoto94@gmail.com>
2022-08-01 22:53:01 -0400

    Conexión mongodb docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -6,6 +7,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), }),
+      MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
     PokemonModule
   ],
 })
