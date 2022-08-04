c77bb8edb58f4204675262cf2b824eb46fab5a99
Thomassr <thomassoto94@gmail.com>
2022-08-03 21:57:13 -0400

    creación de common

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), }),
       MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
-    PokemonModule
+    PokemonModule,
+    CommonModule
   ],
 })
 export class AppModule {}
